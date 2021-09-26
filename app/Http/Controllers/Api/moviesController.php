09dd26b6ed9197110f67a258527e103a45f6a42e
rgpsico <gerro121@hotmail.com>
2021-09-25 22:14:12 -0300

    olha la

--- app/Http/Controllers/Api/moviesController.php
+++ app/Http/Controllers/Api/moviesController.php
@@ -11,96 +11,69 @@ use Illuminate\Support\Facades\Http;
 
 class moviesController extends Controller
 {
-    
-
-    protected $service;
-
-    public function __construct(MovieService $service)
-    {
-        $this->service = $service;
-        
-        
-    }
-    
- public function index() 
- {
-     $popularMovies  = $this->service->allMovieDB();
-     return moviesResource::collection($popularMovies);
-    
- }
 
 
- public function show($id) 
- {
-    $popularMovies =  $this->service->moviesById($id);
-    
-    return $popularMovies;
- }
+   protected $service;
+
+   public function __construct(MovieService $service)
+   {
+      $this->service = $service;
+   }
+
+   public function index()
+   {
+      $popularMovies  = $this->service->allMovieDB();
+      return moviesResource::collection($popularMovies);
+   }
 
 
- public function byGenre($genreId) 
- {
-    $popularMovies =  $this->service->moviesByGenre($genreId);    
-    return moviesResource::collection($popularMovies);
- 
+   public function show($id)
+   {
+      $popularMovies =  $this->service->moviesById($id);
 
- }
+      return $popularMovies;
+   }
+
+
+   public function byGenre($genreId)
+   {
+      $popularMovies =  $this->service->moviesByGenre($genreId);
+      return moviesResource::collection($popularMovies);
+   }
 
 
 
- public function popularmMovies($popular) 
- {   
-     if (!$popularMovies =  $this->service->popularMovies($popular)) {
-        return response()->json(['message' => 'the movie Not Found'], 404);
-    }   
- 
-    return moviesResource::collection($popularMovies);
-   
- }
+   public function popularmMovies($popular)
+   {
+      if (!$popularMovies =  $this->service->popularMovies($popular)) {
+         return response()->json(['message' => 'the movie Not Found'], 404);
+      }
 
- public function releaseDate() 
- {   
-    $popularMovies =  $this->service->moviesLastes();    
-    return moviesResource::collection($popularMovies);
-  
- }
+      return moviesResource::collection($popularMovies);
+   }
 
- public function byTitle($movieByTitle) 
- {   
-    if (!$popularMovies =  $this->service->moviesbyTitle($movieByTitle)) {
-        return response()->json(['message' => 'the movie Not Found'], 404);
+   public function releaseDate()
+   {
+      $popularMovies =  $this->service->moviesLastes();
+      return moviesResource::collection($popularMovies);
+   }
 
-    }   
-    return moviesResource::collection($popularMovies);
- }
+   public function byTitle($movieByTitle)
+   {
+      if (!$popularMovies =  $this->service->moviesbyTitle($movieByTitle)) {
+         return response()->json(['message' => 'the movie Not Found'], 404);
+      }
+      return moviesResource::collection($popularMovies);
+   }
 
 
 
- public function rating($rating) 
- {  
-    if (!$popularMovies =  $this->service->rating($rating)){
-        return response("the movie no serrach",403);
-
-    }   
-    return moviesResource::collection($popularMovies);
- }
-
- 
- 
-
- 
- 
-
-
-
-
- public function teste() 
- {
-    
-   dd($this->service->allMovieDB());
- }
-
-
-
+   public function rating($rating)
+   {
+      if (!$popularMovies =  $this->service->rating($rating)) {
+         return response("the movie Not Found", 403);
+      }
+      return moviesResource::collection($popularMovies);
+   }
 
 }
